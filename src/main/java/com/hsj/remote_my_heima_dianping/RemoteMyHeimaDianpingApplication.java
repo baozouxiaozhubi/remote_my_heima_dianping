fb91b53100e65820e03cf70f8123a6420916a785
灰色 <412032509@qq.com>
2025-01-19 17:24:04 +0800

    Initial commit

---
+++ src/main/java/com/hsj/remote_my_heima_dianping/RemoteMyHeimaDianpingApplication.java
@@ -0,0 +1,13 @@
+package com.hsj.remote_my_heima_dianping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteMyHeimaDianpingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteMyHeimaDianpingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hsj/remote_my_heima_dianping/RemoteMyHeimaDianpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsj.remote_my_heima_dianping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteMyHeimaDianpingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
